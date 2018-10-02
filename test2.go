b9c7532fe05b35231859a7966f379a774365d9f7
bronze21 <32978299+bronze21@users.noreply.github.com>
2018-10-02 15:31:39 +0700

    Add files via upload

---
+++ test2.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var namadepan, namatengah, namabelakang string
+	namadepan, namatengah, namabelakang = "Muhammad", "Bagus", "Harianto"
+
+	fmt.Println(namadepan, namatengah, namabelakang)
+}
